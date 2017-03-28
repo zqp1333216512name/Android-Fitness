19f2b4a81f1d3f5df74c103131e178227610a5f8
ghsxl <sk_ad_test33@163.com>
2017-03-28 22:53:30 +0800

    fix bug

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ﻿# Android-Fitness
+
+2017-03-28:
+	修复一个bug：饮食记录数超过一页时删除后面的记录会闪退（listview的getChildAt()获取的item只有第一页的有效，滚动条往下的item使用时报nullpointer异常，修改为先使用getAdapter后调用适配器的getView获取item）
+	listview的自定义item布局增加一个隐藏textview保存记录的id（与数据表字段相同）用于删除记录时直接使用
+
 健身饮食记录助手，自带离线食品营养数据库
 ![image](https://github.com/ghsxl/Android-Fitness/blob/master/Fitness.jpg)
--- app/src/com/longapp/diet/DietActivity.java
+++ app/src/com/longapp/diet/DietActivity.java
@@ -11,12 +11,14 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DietActivity extends Activity{
 	private ListView listview;
@@ -44,9 +46,9 @@ public class DietActivity extends Activity{
 		Date date = new Date();
 		String time = String.format("%tY%tm%td", date, date, date);
 		Cursor cursor = database.rawQuery("select id as _id, comment, name, count, heat, protein, fat, carbohydrate from diet where time=?", new String[] { time });
-	    SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,R.layout.diet_list_item, cursor,
-	    		new String[] { "comment", "heat", "name", "count", "protein", "fat", "carbohydrate" }, 
-	    		new int[] {R.id.diet_list_item_comment, R.id.diet_list_item_heat, R.id.diet_list_item_name, R.id.diet_list_item_count, 
+	    SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.diet_list_item, cursor,
+	    		new String[] { "_id", "comment", "heat", "name", "count", "protein", "fat", "carbohydrate" }, 
+	    		new int[] {R.id.diet_list_item_id, R.id.diet_list_item_comment, R.id.diet_list_item_heat, R.id.diet_list_item_name, R.id.diet_list_item_count, 
 	    				R.id.diet_list_item_protein, R.id.diet_list_item_fat, R.id.diet_list_item_carbohydrate});
 	    listview.setAdapter(adapter);
 	    Cursor cursor_total = database.rawQuery("select sum(heat) as total from diet where time=?", new String[] { time });
@@ -59,11 +61,13 @@ public class DietActivity extends Activity{
 		this.listview.setOnItemLongClickListener(new OnItemLongClickListener() {
 					        @Override
 					        public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+					        	
 					                new AlertDialog.Builder(DietActivity.this)
 					                        .setItems(R.array.menu, new DialogInterface.OnClickListener() {
 						                                                public void onClick(DialogInterface dialog, int which) {
 						                                                        String[] PK = getResources().getStringArray(R.array.menu);
 						                                                        if(PK[which].equals("删除")) {
+						                                                        	   //Log.i("test", "itemDelete:"+position);
 						                                                               itemDelete(position);
 						                                                        }
 						                                                }
@@ -79,16 +83,23 @@ public class DietActivity extends Activity{
 	}
 	
 	private void itemDelete(int position) {
-		Date date = new Date();
-		String time = String.format("%tY%tm%td", date, date, date);
-		TextView item_comment= (TextView)listview.getChildAt(position).findViewById(R.id.diet_list_item_comment);
-		String comment = item_comment.getText().toString();
-		TextView item_name= (TextView)listview.getChildAt(position).findViewById(R.id.diet_list_item_name);
-		String name = item_name.getText().toString();
-		TextView item_count= (TextView)listview.getChildAt(position).findViewById(R.id.diet_list_item_count);
-		String count = item_count.getText().toString();
-		database.execSQL("delete from diet where time=? and comment=? and name=? and count=?", new String[]{time, comment, name, count});
-		setData();
+		//Log.i("test", "itemdelete...");
+		try{
+			//listview的getChildat只能获取当前页面显示的item,滚动条以下的item获取后使用（第二条语句）会nullpointerexecption
+			//View view = this.listview.getChildAt(position);
+			//TextView item_id = (TextView)view.findViewById(R.id.diet_list_item_id);
+			//改成先获取adapter再取item即可解决问题
+			TextView item_id = (TextView)listview.getAdapter().getView(position, null, null).findViewById(R.id.diet_list_item_id);
+
+			int id = Integer.parseInt(item_id.getText().toString());
+			database.execSQL("delete from diet where id=?", new Integer[]{id});
+			setData();
+		} catch(Exception e) {
+			//Log.i("exception:", ""+e);
+			Toast toast=Toast.makeText(this, "删除出错", Toast.LENGTH_SHORT); 
+			//显示toast信息 
+			toast.show();
+		}
 	}
 	
 	@Override
